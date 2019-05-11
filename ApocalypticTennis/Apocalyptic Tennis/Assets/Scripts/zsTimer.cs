17819c52f77fcc620eaac7ca68d0f197a2a5ff66
ZaCkOX <zacko1000@hotmail.com>
2019-05-10 17:27:00 -0700

    TimerAddForKids

---
+++ ApocalypticTennis/Apocalyptic Tennis/Assets/Scripts/zsTimer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zsTimer : MonoBehaviour {
+
+    public float TimerToPlay = 60f;
+    public GameObject TextGameObject;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Subtract time
+        TimerToPlay -= Time.deltaTime;
+        //Check if time is up
+        if (TimerToPlay <= 0f) {
+            //Enable
+            TextGameObject.SetActive(true);
+        }
+	}
+}
